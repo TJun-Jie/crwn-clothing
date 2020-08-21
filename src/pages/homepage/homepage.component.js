459adb1f4a4e24b4c6fff57522db57d1ccb9ee9f
junjie <tjunjie2000@gmail.com>
2020-08-22 00:45:03 +0800

    Split  homepage into multiple components

--- src/pages/homepage/homepage.component.js
+++ src/pages/homepage/homepage.component.js
@@ -1,39 +1,10 @@
 import React from 'react';
 import './homepage.styles.scss';
+import Directory from '../../components/directory/directory.componet';
+
 const Homepage = () => (
     <div className='homepage'>
-        <div className="directory-menu">
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">HATS</h1>
-                    <h3 className="subtitle">SHOP NOW</h3>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">JACKETS</h1>
-                    <h3 className="subtitle">SHOP NOW</h3>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">SNEAKERS</h1>
-                    <h3 className="subtitle">SHOP NOW</h3>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">MENS</h1>
-                    <h3 className="subtitle">SHOP NOW</h3>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">WOMENS</h1>
-                    <h3 className="subtitle">SHOP NOW</h3>
-                </div>
-            </div>
-        </div>
+        <Directory></Directory>
     </div>
 );
 
