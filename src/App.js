c7a12a344df93c47af3aa68d37cbbd694888c9d9
junjie <tjunjie2000@gmail.com>
2020-08-23 00:04:52 +0800

    Added sign in form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
 
 const HatsPage = () => (
   <div>
@@ -19,6 +20,7 @@ function App() {
         <Route exact path="/" component={Homepage}></Route>
         <Route path="/shop/hats" component={HatsPage}></Route>
         <Route path="/shop" component={ShopPage}></Route>
+        <Route path="/signin" component={SignInAndSignUpPage}></Route>
       </Switch>
     </div>
   );
